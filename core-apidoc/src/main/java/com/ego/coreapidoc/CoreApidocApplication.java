3e6eb78ba62b3f01147ffd6af891e4f71d94abea
ego <157026488@qq.com>
2018-12-23 16:37:56 +0800

    第十二篇：springboot 集成 apidoc

---
+++ core-apidoc/src/main/java/com/ego/coreapidoc/CoreApidocApplication.java
@@ -0,0 +1,14 @@
+package com.ego.coreapidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApidocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApidocApplication.class, args);
+	}
+
+}
+
